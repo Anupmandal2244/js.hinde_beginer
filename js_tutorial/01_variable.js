f5adc7626b9c9383bf25faafd90bf277316ea34f
Anupmandal2244 <145279520+Anupmandal2244@users.noreply.github.com>
2023-09-21 18:59:46 +0000

    variable

---
+++ js_tutorial/01_variable.js
@@ -0,0 +1,15 @@
+const accountid = 12344
+let accountEmail="14458"
+var accountPassword = "15589"
+accountCity = "Kolkata"
+
+// accountid = 256 not allowed
+accountEmail="114477"
+accountPassword = "225588"
+accountCity = "delhi"
+
+
+
+console.log(accountid);
+console.log(accountPassword);
+console.table([accountid,accountEmail,accountPassword,accountCity])
